f94a9684c563627325771e8ed6f921739a6eeb01
loch <nigelloch@gmail.com>
2021-11-27 14:08:01 -0600

    creating variables for password generator function

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+var characterCount = document.getElementById('characterCount')
+var characterRange = document.getElementById('characterRange')
+var characterUppercase = document.getElementById('characterUppercase')
+var characterLowercase = document.getElementById('characterLowercase')
+var characterNumbers = document.getElementById('characterNumbers')
+var characterSymbols = document.getElementById('characterSymbols')
+
+
 
 var randomstring = Math.random().toString(36).slice(-12);
 console.log(randomstring);
