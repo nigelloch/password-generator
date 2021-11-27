f62ab1c45cec5fc046ba778287e0d67a83fc182b
loch <nigelloch@gmail.com>
2021-11-27 12:45:15 -0600

    random number function

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,22 @@
+// Assignment code here
+
+
+var randomstring = Math.random().toString(36).slice(-12);
+console.log(randomstring);
+        
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# password-generator
+# Password Generator Starter Code
+
+Clone the starter code (Links to an external site.).
+
+Modify the code to meet the Acceptance Criteria.
+
+Ensure that your work meets the full list of grading requirements below.
+
+Reach out to your classmates and instructional support team for help if you need it.
+
+Follow the submission instructions.
