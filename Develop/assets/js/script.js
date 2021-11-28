893b2b53abf61b7101eeae50efc268e33bf68f29
loch <nigelloch@gmail.com>
2021-11-27 18:12:06 -0600

    added variables

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -7,6 +7,12 @@ var characterLowercase = document.getElementById('characterLowercase')
 var characterNumbers = document.getElementById('characterNumbers')
 var characterSymbols = document.getElementById('characterSymbols')
 
+var uppercaseCharcodes = LowToHigh(65, 90)
+var lowecarseCharcodes = LowToHigh(97, 122)
+var numberCharcodes = LowToHigh(48,57)
+var symbolCharcodes = LowToHigh(33, 47).concat(LowToHigh(58, 64))
+.concat(LowToHigh(91, 96))
+.concat(LowToHigh(123, 126))
 
 
 var randomstring = Math.random().toString(36).slice(-12);
@@ -18,6 +24,8 @@ console.log(randomstring);
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+function generatePassword(chara)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
